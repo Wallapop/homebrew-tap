edc903049ec60c3f79a8474c13e4f590c57d2eef
Jordi Funollet <funollet@fastmail.fm>
2020-05-29 21:55:21 +0200

    v0.2.0

--- Formula/aws-laziness.rb
+++ Formula/aws-laziness.rb
@@ -1,8 +1,8 @@
 class AwsLaziness < Formula
-  desc ""
+  desc "SSH helpers for AWS EC2/ECS instances"
   homepage "https://github.com/funollet/aws-laziness/"
-  url "https://github.com/funollet/aws-laziness/archive/v0.1.0.tar.gz"
-  sha256 "d414687be5d7395926aaa2b103956531cae0fa9106ea5828dfb5453bd5b481f4"
+  url "https://github.com/funollet/aws-laziness/archive/v0.2.0.tar.gz"
+  sha256 "acf7e6250dc0a5184cadad9265dca7df0d7f25029104b37697bcf264bb12876a"
 
   depends_on "fzf"
   depends_on "jq"
@@ -15,15 +15,7 @@ class AwsLaziness < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test aws-laziness`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    assert_match "Usage: ec2ssh", shell_output("#{bin}/ec2ssh --help")
+    assert_match "Usage: ecsssh", shell_output("#{bin}/ecsssh --help")
   end
 end
