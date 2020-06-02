5ab22c841a917e209897b0e2386b0855a90eda4d
Jordi Funollet <funollet@fastmail.fm>
2020-06-02 15:49:17 +0200

    install v0.2.1

--- Formula/aws-laziness.rb
+++ Formula/aws-laziness.rb
@@ -1,8 +1,8 @@
 class AwsLaziness < Formula
   desc "SSH helpers for AWS EC2/ECS instances"
   homepage "https://github.com/funollet/aws-laziness/"
-  url "https://github.com/funollet/aws-laziness/archive/v0.2.0.tar.gz"
-  sha256 "acf7e6250dc0a5184cadad9265dca7df0d7f25029104b37697bcf264bb12876a"
+  url "https://github.com/funollet/aws-laziness/archive/v0.2.1.tar.gz"
+  sha256 "37828f3641274275f191af71e397cf7ad3fba44096f353de0a696350569c3b34"
 
   depends_on "fzf"
   depends_on "jq"
