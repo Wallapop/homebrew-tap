b3424dabf4e1d40523035944daf156ca5ff62a10
Jordi Funollet <jordi.funollet@wallapop.com>
2021-02-23 16:29:44 +0100

    Use a release artifact from Wallapop's repository

--- Formula/aws-laziness.rb
+++ Formula/aws-laziness.rb
@@ -1,7 +1,7 @@
 class AwsLaziness < Formula
   desc "SSH helpers for AWS EC2/ECS instances"
-  homepage "https://github.com/funollet/aws-laziness/"
-  url "https://github.com/funollet/aws-laziness/archive/v0.2.2.tar.gz"
+  homepage "https://github.com/Wallapop/aws-laziness/"
+  url "https://github.com/Wallapop/aws-laziness/archive/v0.2.2.tar.gz"
   sha256 "10d8282e503dc301cd516af1eb81d8079b290a5cfe0830cf6e11476749707ae6"
 
   depends_on "fzf"
